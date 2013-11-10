e27093945279546a19e13c2d86dc6de7a508732e
Steven Hepting <shepting@gmail.com>
2013-11-09 19:55:23 -0800

    Moved files around.

---
+++ Demo/PSAutolayoutDemo/PSDetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  PSDetailViewController.m
+//  PSAutolayoutDemo
+//
+//  Created by Steven Hepting on 11/9/13.
+//  Copyright (c) 2013 Hepting. All rights reserved.
+//
+
+#import "PSDetailViewController.h"
+
+@interface PSDetailViewController ()
+- (void)configureView;
+@end
+
+@implementation PSDetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Demo/PSAutolayoutDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PSAutolayoutDemo
+//
+//  Created by Steven Hepting on 11/9/13.
+//  Copyright (c) 2013 Hepting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
